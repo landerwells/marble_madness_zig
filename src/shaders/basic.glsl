c687ea3d136d007e55ce203301d7f0daa3fbb377
landerwells <landerwells@gmail.com>
2026-06-01 21:17:34 -0500

    Fixed texture rotation

--- src/shaders/basic.glsl
+++ src/shaders/basic.glsl
@@ -6,13 +6,11 @@ in vec4 position;
 in vec4 color0;
 in vec2 texture0;
 
-out vec4 color;
 out vec2 uv;
 
 
 void main() {
     gl_Position = position;
-    color = color0;
     uv = texture0;
 }
 @end
@@ -21,12 +19,12 @@ void main() {
 layout(binding=0) uniform texture2D tex;
 layout(binding=0) uniform sampler smp;
 
-in vec4 color;
+// in vec4 color;
 in vec2 uv;
 out vec4 frag_color;
 
 void main() {
-    frag_color = texture(sampler2D(tex, smp), uv) * color;
+    frag_color = texture(sampler2D(tex, smp), uv);
 }
 @end
 
