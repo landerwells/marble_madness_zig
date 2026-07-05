9e25d757857bdb9dabe22f421a14d7bff69a006a
landerwells <landerwells@gmail.com>
2026-07-05 00:04:38 -0500

    Working renderer

--- src/app.zig
+++ src/app.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 
 const sokol = @import("sokol");
 const zmath = @import("zmath");
-const shd = @import("shader");
 
 const sapp = sokol.app;
 const sg = sokol.gfx;
@@ -13,15 +12,11 @@ const slog = sokol.log;
 const asset = @import("asset.zig");
 
 const Input = @import("input.zig");
-const Marble = @import("marble.zig");
 const Map = @import("map.zig");
+const Marble = @import("marble.zig");
+const Renderer = @import("renderer.zig");
 
 const App = @This();
-
-const Vertex = struct {
-    position: [2]f32,
-    uv: [2]f32,
-};
 
 const INTERNAL_WIDTH = 320;
 const INTERNAL_HEIGHT = 240;
@@ -113,88 +108,3 @@ pub fn event(ev: ?*const sapp.Event, user_data: ?*anyopaque) callconv(.c) void {
 
     app.input.eventHanlder(e);
 }
-
-const Renderer = struct {
-    bind: sg.Bindings = .{},
-    pip: sg.Pipeline = .{},
-
-    const vertices = [_]Vertex{
-        .{
-            .position = .{ 0.0, 1.0 },
-            .uv = .{ 0.0, 1.0 },
-        },
-        .{
-            .position = .{ 1.0, 0.0 },
-            .uv = .{ 1.0, 0.0 },
-        },
-        .{
-            .position = .{ 1.0, 1.0 },
-            .uv = .{ 1.0, 1.0 },
-        },
-        .{
-            .position = .{ 0, 1.0 },
-            .uv = .{ 0.0, 1.0 },
-        },
-    };
-
-    const indices = [_]u16{ 0, 1, 2, 0, 2, 3 };
-
-    fn init(self: *Renderer) void {
-        self.bind.vertex_buffers[0] = sg.makeBuffer(.{
-            .data = sg.asRange(&vertices),
-        });
-
-        self.bind.index_buffer = sg.makeBuffer(.{
-            .usage = .{ .index_buffer = true },
-            .data = sg.asRange(&indices),
-        });
-
-        // ...and a sampler object with default attributes
-        self.bind.samplers[shd.SMP_smp] = sg.makeSampler(.{
-            .min_filter = .NEAREST,
-            .mag_filter = .NEAREST,
-            .wrap_u = .CLAMP_TO_EDGE,
-            .wrap_v = .CLAMP_TO_EDGE,
-        });
-
-        self.pip = sg.makePipeline(.{
-            .index_type = .UINT16,
-            .shader = sg.makeShader(shd.basicShaderDesc(sg.queryBackend())),
-            .layout = init: {
-                var l = sg.VertexLayoutState{};
-                l.attrs[shd.ATTR_basic_position].format = .FLOAT2;
-                l.attrs[shd.ATTR_basic_texture0].format = .FLOAT2;
-                break :init l;
-            },
-            .colors = init: {
-                var colors: [8]sg.ColorTargetState = @splat(.{});
-                colors[0].blend = .{
-                    .enabled = true,
-                    .src_factor_rgb = .SRC_ALPHA,
-                    .dst_factor_rgb = .ONE_MINUS_SRC_ALPHA,
-                    .src_factor_alpha = .ONE,
-                    .dst_factor_alpha = .ONE_MINUS_SRC_ALPHA,
-                };
-                break :init colors;
-            },
-        });
-    }
-
-    fn draw(self: *Renderer, view: sg.View, _: [2]f32, position: [2]f32) void {
-        // var model: zmath.Mat = zmath.identity();
-        // model = zmath.translation(tmodel, y: f32, z: f32)
-        const model = zmath.translationV(position ++ .{ 0.0, 0.0 });
-        // model = zmath.translation(model, 0.5 * size[0], 0.5 * size[1]);
-        // model = zmath.translation(model, -0.5 * size[0], 0.5 * size[1]);
-
-        const uniforms = shd.VsParams{
-            .model = zmath.matToArr(model),
-            .projection = zmath.matToArr(zmath.orthographicLhGl(8, 6, -1.0, 1.0)),
-        };
-
-        self.bind.views[shd.VIEW_tex] = view;
-        sg.applyBindings(self.bind);
-        sg.applyUniforms(shd.UB_vs_params, sg.asRange(&uniforms));
-        sg.draw(0, 6, 1);
-    }
-};
--- src/asset.zig
+++ src/asset.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 
+const stb_image = @cImport("../lib/stb_image.h");
 const sokol = @import("sokol");
 
 const sg = sokol.gfx;
@@ -7,13 +8,13 @@ const sg = sokol.gfx;
 const zigimg = @import("zigimg");
 
 pub fn loadImage(io: std.Io, allocator: std.mem.Allocator, path: []const u8) !sg.Image {
-    // need a read buffer.
     var read_buffer: [zigimg.io.DEFAULT_BUFFER_SIZE]u8 = undefined;
     var image = try zigimg.Image.fromFilePath(allocator, io, path, read_buffer[0..]);
     defer image.deinit(allocator);
 
     try image.convert(allocator, .rgba32);
 
+    // Write something to swap the pixels on the this boi
     const pixels = image.pixels.rgba32;
 
     var img_data = sg.ImageData{};
